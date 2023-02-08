a366bd6424e0e0bae496b898a7261ee2ba5f4d70
FourthRome <romansdidnotcrucify@gmail.com>
2023-02-08 13:25:47 +0300

    Add custom (lazy) comparator

--- sort_file.cpp
+++ sort_file.cpp
@@ -3,6 +3,11 @@
 #include <fstream>
 #include <vector>
 
+
+bool compare(const std::string& a, const std::string& b) {
+    return a.compare(0, b.size(), b);
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         std::cerr << "Incorrect number of arguments; 1 expected, got "
@@ -25,7 +30,16 @@ int main(int argc, char **argv) {
     in_stream.close();
 
     // Approach A: reuse what works well already
+    // std::sort(result.begin(), result.end());
+
+    // Approach B: a little less lazy, custom comparator
     std::sort(result.begin(), result.end());
+
+    // Approach C: custom comparator (compare string symbol-by-symbol)
+    // TODO
+
+    // Approach D: custom comparator, custom sorting implementation
+    // TODO
 
     for (auto item : result) {
         std::cout << item << std::endl;
